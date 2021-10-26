d1c1c9b110cb0418f5c9a0416562c9bf8c7a4fcd
Kathrine Hermansen <kath.hermansen@gmail.com>
2021-10-26 12:31:57 +0200

    endret en kommentar

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,21 +1,30 @@
-import React from "react"; 
+import React from "react";
 
 const Login = () => {
+  return (
+    <div className="inputDiv">
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Password" />
 
-    return (
-        <div className="inputDiv">
-            <input type="text" placeholder="Username"/>
-            <input type="text" placeholder="Password"/>
+      <div className="wrap">
+        <div className="line" />
+        <p>or</p>
+        <div className="line" />
+      </div>
 
-            <div className="wrap">
-            <div className="line"/><p>or</p><div className="line"/></div>
+      <button className="loginButton">Log in with Facebook</button>
+      <button className="loginButton">Log in with Google</button>
+      <p>
+        <a href="/register">Don't have an account? Register</a>
+      </p>
+    </div>
+  );
+};
 
-            <button className="loginButton">Log in with Facebook</button>
-            <button className="loginButton">Log in with Google</button>
-            <p><a href="/register">Don't have an account? Register</a></p>
-        </div>
+export default Login;
 
-    ); 
-}
-
-export default Login; 
+/**
+ *
+ * hello
+ *
+ */
