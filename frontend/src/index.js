02f93954a5ee742e0e1484bac2a3f4383cfb0f84
Øystein H. Vikane <hvikane94@gmail.com>
2021-11-02 17:06:21 +0100

    added sass & scss, removed some errors and fixed structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import './styling/index.scss';
 
 import Routes from "./Routes";
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -24,12 +23,3 @@ class Index extends React.Component {
 }
 
 ReactDOM.render(<Index />, document.getElementById("root"));
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-//<App />
