1e87af2eaf73a92d3b3f7c3fd277f76f2abd83ea
vildekristine99 <578051@stud.hvl.no>
2021-11-17 11:16:43 +0100

    Changes by KathrineH

--- src/main/java/no/hvl/dat250/Votesphere/VotesphereApplication.java
+++ src/main/java/no/hvl/dat250/Votesphere/VotesphereApplication.java
@@ -3,13 +3,10 @@ package no.hvl.dat250.Votesphere;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class VotesphereApplication {
 
-	
     public static void main(String[] args) {
         SpringApplication.run(VotesphereApplication.class, args);
     }
-
 }
