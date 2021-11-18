f97143adc7917a66f5f7d67e046cde73afade0f4
vildekristine99 <578051@stud.hvl.no>
2021-11-18 20:12:31 +0100

    noting

--- App/frontend/src/Components/Header.jsx
+++ App/frontend/src/Components/Header.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { useCallback } from "react";
+import AuthService from "../Service/auth.service";
 
 class Header extends React.Component {
+    
+    
     render() {
         return (
             <div className = "header">
                 <div className="title">
                     <h1>Votesphere</h1>
                 </div>
-                    <div className="login"><a href={this.props.link} >{this.props.text}</a></div>
+                    <div className="login"><a href={this.props.link}>{ this.props.text }</a></div>
             </div>
         )
     }
+
     
  
 }
