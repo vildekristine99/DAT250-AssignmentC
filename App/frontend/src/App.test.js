99a60421f6f6eec3122417c411615805f9127287
Øystein H. Vikane <31279370+Oysteinvikane@users.noreply.github.com>
2021-11-19 07:02:40 +0100

    Update App.test.js

--- App/frontend/src/App.test.js
+++ App/frontend/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import Register from "./Components/Register.jsx";
 
-test('renders navigation bar', () => {
-    render(<NavigationBar />);
+test('renders register', () => {
+    render(<Register />);
     expect(screen.getByText(/register)).toBeInTheDocument();
 });
