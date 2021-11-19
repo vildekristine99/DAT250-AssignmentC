abb7913eab97fbda1978f9dcfa4c5a412e600417
Øystein H. Vikane <31279370+Oysteinvikane@users.noreply.github.com>
2021-11-19 07:02:11 +0100

    Update App.test.js

--- App/frontend/src/App.test.js
+++ App/frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Register from "./Components/Register.jsx";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders navigation bar', () => {
+    render(<NavigationBar />);
+    expect(screen.getByText(/register)).toBeInTheDocument();
 });
